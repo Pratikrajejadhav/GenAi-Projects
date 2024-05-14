f136e6605ca93f2911c926c562feb603d8c94093
Pratik <pratikrajejadhav2002@gmail.com>
2024-05-14 16:06:46 +0530

    genai

---
+++ Images_analysis.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import google.generativeai as genai
+from PIL import Image
+import matplotlib.pyplot  as plt 
+
+api_key = "AIzaSyD9Tj4yxSUTFYRZaFtPnqCaiWUgMW3m4J4"
+genai.configure(api_key=api_key)
+
+# st.title ("Application")
+# st.text("streamlit application")
+# st.write("hello im creating an app")
+# st.text_input("name"," ")
+# st.button("click")
+
+st.header("Image Analytics")
+upload_file = st.file_uploader("Upload image", type=["png","jpg","jpeg"])
+
+if upload_file is not None:
+    st.image(Image.open(upload_file))
+
+
+prompt = st.text_input("Enter the text")
+
+
+if st.button("GET RESPONSE"):
+    img = Image.open(upload_file)
+    model=genai.GenerativeModel("gemini-1.0-pro-vision-latest")
+    response=model.generate_content([prompt,img])
+    st.markdown(response.text)
